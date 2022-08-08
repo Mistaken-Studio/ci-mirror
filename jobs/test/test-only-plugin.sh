93cc34bfab3dbcd1598f2417b691af306b654d1e
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-08-08 17:12:21 +0000

    Update test-only-plugin.sh

--- jobs/test/test-only-plugin.sh
+++ jobs/test/test-only-plugin.sh
@@ -13,6 +13,7 @@ mv ./.config/dependencies/Exiled.Loader.dll ./.config/EXILED/
 #Dependencies
 mv ./.config/dependencies/MCS-Client.dll ./.config/EXILED/Plugins/dependencies/
 mv ./.config/dependencies/MCS-Shared.dll ./.config/EXILED/Plugins/dependencies/
+mv ./.config/dependencies/Google.Protobuf.dll ./.config/EXILED/Plugins/dependencies/
 mv ./.config/dependencies/Discord_Webhook.dll ./.config/EXILED/Plugins/dependencies/
 mv ./.config/dependencies/NetCoreServer.dll ./.config/EXILED/Plugins/dependencies/
 mv ./.config/dependencies/Exiled.API.dll ./.config/EXILED/Plugins/dependencies/
